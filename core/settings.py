e564ed5b0cd9b7516274e46a421b2b9144dcb819
Allan Camargo <allan.mdev@gmail.com>
2024-12-24 09:01:13 -0300

    fix: adjust requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', True)
 
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost').split(',')
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'http://localhost').split(',')
 
 
 # Application definition
